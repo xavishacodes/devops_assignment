d99a42606203a83b370223c02a002de982adf170
anamikabits <161862798+anamikabits@users.noreply.github.com>
2024-03-15 23:39:36 +0530

    Add files via upload
    feature : Adding login/logout

---
+++ src/main/java/com/example/demo/Service/EmployeeService.java
@@ -0,0 +1,54 @@
+package com.example.demo.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.stereotype.Service;
+
+import com.example.demo.Modal.EmployeeModal;
+
+import com.example.demo.Dao.employeeDao;
+
+@Service
+public class EmployeeService {
+
+	@Autowired
+	private employeeDao employeedao;
+
+	public void addEmployee(EmployeeModal e) {
+		employeedao.save(e);
+	}
+
+	public List<EmployeeModal> getAllEmployee() {
+
+		return employeedao.findAll();
+	}
+
+	public void deleteEmployee(int id) {
+		employeedao.deleteById(id);
+	}
+
+	public EmployeeModal getEMpById(int id) {
+		Optional<EmployeeModal> employeeModal = employeedao.findById(id);
+
+		if (employeeModal.isPresent()) {
+			return employeeModal.get();
+
+		}
+
+		return null;
+	}
+
+	public Boolean findEmployeee(EmployeeModal e) {
+		Optional<EmployeeModal> employeeModal = employeedao.findById(e.getId());
+
+		if (employeeModal.isPresent()) {
+			return true;
+
+		}
+
+		return false;
+	}
+}
